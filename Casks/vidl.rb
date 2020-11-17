afcaa35473985c54378cb228d1637e9f1fc241d2
Roman Levin <romanlevin@gmail.com>
2020-11-17 17:30:12 +0100

    Add ViDL Latest

---
+++ Casks/vidl.rb
@@ -0,0 +1,11 @@
+cask "vidl" do
+  version :latest
+  sha256 :no_check
+
+  url "https://omz-software.com/vidl/ViDL.zip"
+  name "ViDL"
+  desc "GUI for youtube-dl"
+  homepage "https://omz-software.com/vidl/"
+
+  app "ViDL.app"
+end
